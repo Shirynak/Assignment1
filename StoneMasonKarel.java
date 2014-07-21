14a725f1cb0676d908e7da0c0717b1a46e9012de
S <s>
2014-07-21 19:30:10 +0200

    {"type":"TI", "runs":"1"}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -3,61 +3,18 @@ import stanford.karel.*;
 public class StoneMasonKarel extends SuperKarel {
 
 	public void run() {
-		putBeeper();
-		turnLeft();
-		move();
-		putBeeper();
-		move(); 
-		putBeeper();
-		turnRight();
-		move();
-		move();
-		move();
-		move();
-		putBeeper();
-		move();
-		turnLeft();
-		move();
-		move();
-		turnLeft();
-		move();
-		putBeeper();
-		turnAround();
-		move();
-		move();
-		move();
-		turnRight();
-		move();
-		turnLeft();
-		move();
-		putBeeper();
-		move();
-		turnRight();
-		move();
-		move();
-		turnRight();
-		move();
-		putBeeper();
-		turnLeft();
-		move();
-		putBeeper();
-		turnLeft();
-		move();
-		move();
-		move();
-		turnLeft();
-		move();
-		turnRight();
-		move();
-		putBeeper();
-		turnAround();
-		move();
-		turnRight();
-		move(); 
-		move();
-		turnRight();
-		move();
-		putBeeper();
+		fillPothole();
+		
 	}
-
+	private void fillPothole() {
+		if (noBeepersPresent()) {
+			putBeeper();
+		} else {
+			move();
+			move();
+			move();
+			move();
+		}
+	}
+	
 }
