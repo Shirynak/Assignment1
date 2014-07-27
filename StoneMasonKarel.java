b41096d1b5b2feb6f4cfcee36ab7c56c350742cc
S <s>
2014-07-27 20:08:38 +0200

    {"type":"TI", "runs":"1"}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -4,54 +4,16 @@ public class StoneMasonKarel extends SuperKarel {
 
 	public void run() {
 		turnLeft();
-		putBeepers();
-		moveToNextPillow();
-		putBeepers();
-		moveToNextPillow();	
-	}
-	private void putBeepers() {
-		for (int i = 0; i < 4; i++) 
-			moveAlongA();
+		while (frontIsClear()) {
+			move(); if (noBeepersPresent()) {
+				putBeeper();
+			} else { 
+				move();
+				
+			}
 		
-	}
-	private void moveAlongA() {
-		if (noBeepersPresent()) {
-			putBeeper();
-			move();
-		} else { 
-			move();
 		}
 	}
-	
-	private void moveAlongD() {
-		if (beepersPresent()) {
-			move();
-		} else { 
-			putBeeper();
-			move();
-		if (facingSouth()) {
-			turnLeft();
-		}
-		}
-		
-	}
-	
-	private void moveToNextPillow() {
-		if (frontIsBlocked()) {
-			turnRight();
-		} else {
-			turnLeft();
-		}
-		move();
-		move();
-		move();
-		move();
-		if (frontIsClear()) {
-			turnRight();
-	
-		}
-	}
-	
 		
 	
 }
